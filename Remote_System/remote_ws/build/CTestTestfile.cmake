37f1542119086196a443ce97e5cd485f97e455cb
Shubhojyoti <shubho.important@gmail.com>
2015-01-25 15:12:31 +0530

    Worked on Simulation using Gazebo

---
+++ Remote_System/remote_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/shubo/Desktop/Blind_Localization/sangi1.0/Remote_System/remote_ws/src
+# Build directory: /home/shubo/Desktop/Blind_Localization/sangi1.0/Remote_System/remote_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ Remote_System/remote_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/shubo/Desktop/Blind_Localization/sangi1.0/Remote_System/remote_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
