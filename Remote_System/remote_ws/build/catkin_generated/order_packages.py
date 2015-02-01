79ea595075826f2c30b77add8a070eb1b3f1e9a1
Shubhojyoti <gangulyshubhojyoti@gmail.com>
2015-02-01 19:50:32 +0530

    Binaries removed from commit

--- Remote_System/remote_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/shubo/Desktop/Blind_Localization/sangi1.0/Remote_System/remote_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/shubo/auro_repo/sandbox/devel;/opt/ros/indigo".split(';') if "/home/shubo/auro_repo/sandbox/devel;/opt/ros/indigo" != "" else []
--- Remote_System/remote_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/shubo/Desktop/Blind_Localization/sangi1.0/Remote_System/remote_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
