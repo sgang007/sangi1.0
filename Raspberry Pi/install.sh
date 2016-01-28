a421cc1c0b9321ef157e12fba77bf9530dc9c996
sgang007 <gangulyshubhojyoti@gmail.com>
2016-01-28 12:00:57 +0000

    VR server modification added

--- Raspberry Pi/install.sh
+++ Raspberry Pi/install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 sudo apt-get install apache2
+sudo sh PiCam_server/RPi_Cam_Web_Interface_Installer.sh
+dialog --inputbox "Enter path to PiCam Server:\n(eg:- http://localhost/piviewer)" 8 40 2>temp
+ip=$(cat temp)
+
+sed '93 c \          piImage.src = "http://localhost/'$ip'/cam_pic.php?time=" + new Date().getTime();' ./VR_server/index.html
+
