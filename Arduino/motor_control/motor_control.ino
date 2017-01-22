eb50f30b57d052365410539562dc2a27c2e0bdbe
sgang007 <shubho.important@gmail.com>
2017-01-22 19:14:53 +0530

    Added brake function with better commenting

--- Arduino/motor_control/motor_control.ino
+++ Arduino/motor_control/motor_control.ino
@@ -26,17 +26,25 @@
 #define encA2       4
 #define encB2       2
 
+#define FVEL_STEP 10
+#define AVEL_STEP 1
+#define WHEEL_DIST 30
+#define MAX_SPEED 100
+#define SPEED_FACTOR 400/MAX_SPEED
 
 DualVNH5019MotorShield md(InA1,InB1,ENDIAG1,CS1,InA2,InB2,ENDIAG2,CS2);
 Encoder en1(encA1,encB1);
 Encoder en2(encA2,encB2);
 int enc1_rpm,enc2_rpm;
+int speed1,speed2,forward_vel,angular_vel;
+char c;
 
 void setup() {
     md.init();
   
     Serial.begin(115200);
     Serial.println("Motor Driver and Encoder Test \n --------------------------");
+    speed1=speed2=forward_vel=angular_vel=0;
 
 }
 void stopIfFault()
@@ -44,11 +52,13 @@ void stopIfFault()
   if (md.getM1Fault())
   {
     Serial.println("M1 fault");
+    md.setSpeeds(0,0);
     while(1);
   }
   if (md.getM2Fault())
   {
     Serial.println("M2 fault");
+    md.setSpeeds(0,0);
     while(1);
   }
 }
@@ -56,44 +66,85 @@ void stopIfFault()
 void loop() {
   // put your main code here, to run repeatedly:
 
-      md.setSpeeds(10,10 );
-      //md.setM1Speed(50);
-      //md.setM2Speed(200);
+  stopIfFault();
+  // Read encoder1 outputs and display on screen    
   
   if (en1.read()>COUNTS_PER_REV|| en1.read()<-COUNTS_PER_REV)
   {
     //1 Revoultion complete
     enc1_rpm++;
-    Serial.print("Left Motor(Revs): ");
-    Serial.print(enc1_rpm);
     
     if (en1.read()<0)
-    Serial.print(" anti-clockwise");
+    Serial.print(-enc1_rpm);
     else
-    Serial.print("clockwise");
+    Serial.print(enc1_rpm);
     
     en1.write(0);
     
-    Serial.println();
+    Serial.print("\t");
   }
   
+  //Read encoder 2 outputs and display on screen
   
   if (en2.read()>COUNTS_PER_REV|| en2.read()<-COUNTS_PER_REV)
   {
-    //1 Revoultion complete
+    
     enc2_rpm++;
-    Serial.print("\t Right Motor(Revs): ");
-    Serial.print(enc2_rpm);
     
     if (en2.read()<0)
-    Serial.print(" anti-clockwise");
+    Serial.print(enc2_rpm);
     else
-    Serial.print("clockwise");
+    Serial.print(-enc1_rpm);
     
     en2.write(0);
-    
     Serial.println();
+    
   }  
+
+//================================================================================
+ //Check for Serial Input to control the robot
+  if(Serial.available()){
+    c = Serial.read();
+    switch(c)
+    {
+      case 'w':
+      {
+        forward_vel += FVEL_STEP;
+        break;
+      }
+      case 's':
+      {
+        forward_vel -= FVEL_STEP;
+        break;
+      }
+      case 'a':
+      {
+        angular_vel += AVEL_STEP;
+        break;
+      }
+      case 'd':
+      {
+        angular_vel -= AVEL_STEP;
+        break;
+      }
+      case ' ':
+      {
+         angular_vel=0;
+         forward_vel=0;
+         break;
+      }
+      default:
+      Serial.println("Invalid Input. Press w/a/s/d");
+      
+      
+      }
+      speed1 = (2*forward_vel + angular_vel * WHEEL_DIST) /2;
+      speed2 = (2*forward_vel - angular_vel * WHEEL_DIST) /2 ;
+
+      md.setSpeeds(SPEED_FACTOR*speed1,SPEED_FACTOR*speed2);
+      
+    }
+    
+}
   
 
-}
