a12915cb90ebded1b55b0e0c06eae37242f68bec
sgang007 <shubho.important@gmail.com>
2017-01-25 01:11:27 +0530

    Template code for floating point inputs froms serial

--- Arduino/motor_control/motor_control.ino
+++ Arduino/motor_control/motor_control.ino
@@ -30,6 +30,7 @@
 
 
 //TODO: Read all tuning parameters from EEPROM
+#define INPUT_SIZE 30
 #define FVEL_STEP 10
 #define AVEL_STEP 1
 #define WHEEL_DIST 30
@@ -194,6 +195,35 @@ void teleoperateFromSerial()
   
 }
 
+
+void getRobotState()
+{
+    char input[INPUT_SIZE + 1];
+    byte size = Serial.readBytes(input, INPUT_SIZE);
+    // Add the final 0 to end the C string
+    input[size] = 0;
+    
+    // Read each command pair 
+    char* command = strtok(input, "&");
+    while (command != 0)
+    {
+        // Split the command in two values
+        char* separator = strchr(command, ':');
+        if (separator != 0)
+        {
+            // Actually split the string in 2: replace ':' with 0
+            *separator = 0;
+            int servoId = atoi(command);
+            ++separator;
+            int position = atoi(separator);
+    
+            // Do something with servoId and position
+        }
+        // Find the next command in input string
+        command = strtok(0, "&");
+    }
+    
+}
 
 void printRobotState()
 {
